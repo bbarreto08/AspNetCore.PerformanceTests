8084ccf764187d600771c288a896bc0cacd5878a
Bruno <brunocbarreto2012@gmail.com>
2021-01-17 14:11:52 -0300

    Projeto para avaliar o uso de métodos assíncronos

---
+++ AspNetCore.PerformanceTests/CoffeeTask/Models/Egg.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeTask.Models
+{
+    public class Egg
+    {
+    }
+}
---
+++ AspNetCore.PerformanceTests/CoffeeTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using CoffeeTask.Services;
+using System;
+using System.Diagnostics;
+
+namespace CoffeeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            // Preparando café da manhã usando Threads
+            new CoffeeAsyncService().PrepareAsyncCoffee();
+            stopwatch.Stop();
+            Console.WriteLine($"Tempo passado - Café usando threads: {stopwatch.Elapsed}");
+
+            stopwatch.Restart();
+
+
+            // Preparando café sem uso de threads
+            new CoffeeService().PrepareCoffee();
+
+            stopwatch.Stop();
+            Console.WriteLine($"Tempo passado - Café sem threads: {stopwatch.Elapsed}");
+
+            Console.Read();
+        }
+
+    }
+}
